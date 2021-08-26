8232a15480a2f0d94bdff0b0d56be74c569dc34f
Alexander Botwinick <alexbotwinick@gmail.com>
2021-08-26 12:00:43 +0200

    factor w/ toolkit, export slices to separate files

--- src/store/index.js
+++ src/store/index.js
@@ -1,50 +1,31 @@
 // const redux = require('redux'); // node import
 // import redux from 'redux';
 // import { createStore, combineReducers } from 'redux';
-import { createSlice, configureStore } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialStateCounter = {
-  counter: 0,
-  showCounter: true,
-};
+import counterReducer from './counter-slice';
+import authReducer from './auth-slice';
 
 // preparing slice of global state. every slice needs a name
 // createSlice uses another package called immer(?) to rewrite code we write that seems to mutate state -- immer translates it into immutable codes
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState: initialStateCounter,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload.amount;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialStateAuth = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: 'authentication',
-  initialState: initialStateAuth,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
+// const counterSlice = createSlice({ // moved to separate file!
+//   name: 'counter',
+//   initialState: initialStateCounter,
+//   reducers: {
+//     increment(state) {
+//       state.counter++;
+//     },
+//     decrement(state) {
+//       state.counter--;
+//     },
+//     increase(state, action) {
+//       state.counter = state.counter + action.payload.amount;
+//     },
+//     toggleCounter(state) {
+//       state.showCounter = !state.showCounter;
+//     },
+//   },
+// });
 
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === 'INCREMENT') {
@@ -81,10 +62,7 @@ const authSlice = createSlice({
 // const store = createStore(counterReducer);
 
 const store = configureStore({
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
-
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 
 export default store;
